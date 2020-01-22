a1e828c9d1542b720117c6efeeccb87878a69e46
Ionut Benyi <ionut.benyi@altom.com>
2020-01-22 10:56:54 +0200

    command line tests

---
+++ alttrashcatj/src/test/java/altunity/alttrashcatj/MainMenuTest.java
@@ -0,0 +1,51 @@
+package altunity.alttrashcatj;
+
+import altunity.alttrashcatj.configreader.PropFileReader;
+import altunity.alttrashcatj.pages.MainMenuPage;
+import altunity.alttrashcatj.pages.StartPage;
+import org.junit.*;
+import ro.altom.altunitytester.AltUnityDriver;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
+public class MainMenuTest {
+
+    private static AltUnityDriver driver;
+    private static StartPage startPage;
+    private static MainMenuPage mainMenuPage;
+
+    @BeforeClass
+    public static void setUp() throws IOException {
+        PropFileReader properties = new PropFileReader();
+        driver = new AltUnityDriver(properties.getDeviceIP(), 13000,";","&",true);
+    }
+
+    @Before
+    public void loadLevel(){
+        mainMenuPage = new MainMenuPage(driver);
+        mainMenuPage.loadScene();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        driver.stop();
+        Thread.sleep(1000);
+    }
+
+
+    @Test
+    public void TestMainPageLoadedCorrectly(){
+
+        mainMenuPage.setCharacterName();
+        mainMenuPage.setLeaderBoardButton();
+        mainMenuPage.setMissionButton();
+        mainMenuPage.setRunButton();
+        mainMenuPage.setSettingsButton();
+        mainMenuPage.setStoreButton();
+        mainMenuPage.setThemeName();
+        assertTrue(mainMenuPage.isDisplayed());
+    }
+
+}
